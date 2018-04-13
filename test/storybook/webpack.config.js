330e65925b328725f0267faa305d4d66e3e602b3
malfarokn <malfaro@openknowledgenetwork.com>
2018-04-13 09:55:17 +0200

    Storybook re-fix for path alias

--- test/storybook/webpack.config.js
+++ test/storybook/webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 
-
 module.exports = storybookBaseConfig => {
-  storybookBaseConfig.resolve = {
-    ...storybookBaseConfig.resolve,
-    alias: {
-      ...storybookBaseConfig.resolve.alias,
-      '@': path.join(__dirname, '../../src')
+  return {
+    ...storybookBaseConfig,
+    resolve: {
+      ...storybookBaseConfig.resolve,
+      alias: {
+        ...storybookBaseConfig.resolve.alias,
+        '@': path.join(__dirname, '../../src')
+      }
     }
   };
-  return storybookBaseConfig;
 };
